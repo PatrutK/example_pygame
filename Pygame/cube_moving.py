5898fe389767e667735b5ea88cbba5b6f9ea5111
PatrutK <utkin.best.no@gmail.com>
2020-12-05 15:29:00 +0300

    Куб, который перемещается стрелочка и прыгает с пробелом

--- Pygame/cube_moving.py
+++ Pygame/cube_moving.py
@@ -4,19 +4,18 @@ if __name__ == '__main__':
     pygame.init()
     size = width, height = 800, 800
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Перетаскивание')
-
+    pygame.display.set_caption('Двигающийся куб')
     clock = pygame.time.Clock()
-    running = True
-    rect_x = rect_y = 25
+
+    rect_x, rect_y = 25, 800 - 50
     rect_size = 50
-    rect_color = pygame.Color('green')
-    drag = False
-    isJump = False
     jumpCount = 10
-    v = 1
+    v = 5
+    isJump = False
+    running = True
 
     while running:
+        clock.tick(100)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -32,19 +31,18 @@ if __name__ == '__main__':
                 rect_y += v
             if keys[pygame.K_SPACE]:
                 isJump = True
-        # else:
-        #     if jumpCount >= -10:
-        #         if jumpCount < 0:
-        #             print(jumpCount)
-        #             rect_y += (jumpCount * 2) / 2
-        #         else:
-        #             rect_y -= (jumpCount * 2) / 2
-        #         jumpCount -= 1
-        #     else:
-        #         isJump = False
-        #         jumpCount = 10
+        else:
+            if jumpCount >= -10:
+                if jumpCount < 0:
+                    rect_y += (jumpCount ** 2) / 2
+                else:
+                    rect_y -= (jumpCount ** 2) / 2
+                jumpCount -= 1
+            else:
+                isJump = False
+                jumpCount = 10
 
         screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_size, rect_size))
+        pygame.draw.rect(screen, (255, 255, 255), (rect_x, rect_y, rect_size, rect_size))
         pygame.display.flip()
     pygame.quit()
