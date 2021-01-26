5ae956ca3145393022a18f11d32c41173de0f78e
PatrutK <utkin.best.no@gmail.com>
2021-01-26 21:03:14 +0300

    к щелчку

--- Pygame/Following_the_click.py
+++ Pygame/Following_the_click.py
@@ -4,15 +4,19 @@ if __name__ == '__main__':
     pygame.init()
     size = width, height = 501, 501
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Шарики')
+    pygame.display.set_caption('Следование за шариком')
 
     running = True
+    move = False
 
     color = pygame.Color('red')
     radius = 20
     clock = pygame.time.Clock()
-    c_x = width / 2
-    c_y = height / 2
+    x_pos = width / 2
+    y_pos = height / 2
+    v = 10
+    x, y = 0, 0
+    dx, dy = 0, 0
 
     while running:
         # внутри игрового цикла ещё один цикл
@@ -24,12 +28,20 @@ if __name__ == '__main__':
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = event.pos
-
+                move = True
         # отрисовка и изменение свойств объектов
         screen.fill((0, 0, 0))
-        pygame.draw.circle(screen, color, (c_x, c_y), radius)
-
-        clock.tick(100)
+        pygame.draw.circle(screen, (255, 0, 0), (x_pos, y_pos), radius)
+        # pygame.draw.line(screen, (255, 0, 0), (0, height / 2), (width, height / 2), width=1)
+        if move:
+            dx = x_pos - x
+            dy = y_pos - y
+            # print(x_pos, x)
+            # move = False
+            if x_pos > x:
+                x_pos -= v * clock.tick() / 100  # v * t в секундах
+            elif x_pos < x:
+                x_pos += v * clock.tick() / 100  # v * t в секундах
         # обновление экрана
         pygame.display.flip()
     pygame.quit()
