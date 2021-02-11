67fee33abd087c8ea6c771bbd7ae2174aa11a340
PatrutK <utkin.best.no@gmail.com>
2021-02-11 19:46:52 +0300

    Задача №5

---
+++ Pygame2/к щелчку.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+pygame.init()
+size = 800, 800
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('К щелчку')
+
+running = True
+move = False
+
+x, y = 400, 400
+x_pos, y_pos = x, y
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            move = True
+            x_pos, y_pos = event.pos[0], event.pos[1]
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, pygame.color.Color('red'), (x, y), 60)
+
+    if x_pos > x:
+        x += 1
+    elif x_pos < x:
+        x -= 1
+    if y_pos > y:
+        y += 1
+    elif y_pos < y:
+        y -= 1
+
+    clock.tick(60)
+    pygame.display.flip()
+pygame.quit()
